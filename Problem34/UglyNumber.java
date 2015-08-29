13458df225265af1243673c3e6ccfcfb93a51145
xiehongm <1044203959@qq.com>
2015-08-29 11:07:12 +0800

    Update UglyNumber.java

--- Problem34/UglyNumber.java
+++ Problem34/UglyNumber.java
@@ -1,6 +1,31 @@
 package Problem34;
 
-public class UglyNumber {
+public int getUglyNumber_Solution2(int index){  
+        if(index <0)  
+            return 0;  
+        int[] uglyArray = new int[index];  
+        uglyArray[0] = 1;  
+        int multiply2 = 0;  
+        int multiply3 = 0;  
+        int multiply5 = 0;  
+        for(int i = 1;i<index;i++){  
+            int min = min(uglyArray[multiply2]*2,uglyArray[multiply3]*3,uglyArray[multiply5]*5);  
+            uglyArray[i] = min;  
+            while(uglyArray[multiply2]*2 == uglyArray[i])  
+                multiply2++;  
+            while(uglyArray[multiply3]*3 == uglyArray[i])  
+                multiply3++;  
+            while(uglyArray[multiply5]*5 == uglyArray[i])  
+                multiply5++;  
+        }  
+        return uglyArray[index-1];  
+    }  
+    public int min(int number1,int number2,int number3){  
+        int min = (number1<number2)?number1:number2;  
+        return min <number3?min:number3;  
+    }  
+
+/*public class UglyNumber {
 	/*
 	 * 只包含因子2,3,5的数为丑数，1是第一个丑数，求从小到大的第1500个丑数
 	 */
@@ -32,4 +57,4 @@ public class UglyNumber {
 		return min<number3?min:number3;
 	}
 
-}
+}*/
